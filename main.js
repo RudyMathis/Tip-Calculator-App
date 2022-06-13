2cacec5cb8a4c1c15b386a6e308b1b871682e1d0
RudyMathis <103608310+RudyMathis@users.noreply.github.com>
2022-06-12 20:42:10 -0400

    built out most of html and css still need responisiveness need to make js functional

---
+++ main.js
@@ -0,0 +1,8 @@
+// all fields need to be filled
+// numbers can only reach 2 decimeal places
+// numbers can only input numbers and 1 period
+// do math based on % button
+// bill / people * percent = tip
+// (bill / numbOfPeople) + (percent * (bill/numb0People)) = total
+// if any interaction remove disable from reset
+// reset button sets all fields to inital
