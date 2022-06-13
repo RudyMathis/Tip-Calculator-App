ad0f606b73f9f238f71a51707f899269be9177a2
RudyMathis <103608310+RudyMathis@users.noreply.github.com>
2022-06-13 16:02:08 -0400

    input value into fields output their value to total amount field still need the math function to properly get totals and need to add tip amount function

--- main.js
+++ main.js
@@ -6,3 +6,68 @@
 // (bill / numbOfPeople) + (percent * (bill/numb0People)) = total
 // if any interaction remove disable from reset
 // reset button sets all fields to inital
+
+const bill = document.querySelector("#bill");
+const totalJS = document.querySelector("#total-js");
+const tipJS = document.querySelector("#tip-js");
+const tipBtn = document.querySelectorAll(".tip-btn");
+const numbOfPeople = document.querySelector("#number-people");
+
+
+// gets bill input and put it in total\
+function billFunction(){
+
+    bill.addEventListener("keyup",(e)=>{
+        // totalJS.innerHTML = `${e.target.value}`;
+        // console.log(e.target.value * .05)
+        // console.log(e.target.value)
+        billTotal = e.target.value;
+        totalFunction(billTotal);
+    });
+};
+
+
+
+
+// gets tip input
+function tipFunction(){
+
+    tipBtn.forEach((btn) =>{
+        btn.addEventListener("click",()=>{
+            
+            // tipJS.innerHTML = `${btn.innerHTML.replace(/%/g,'')}`
+            // billTip = btn.innerHTML.replace(/%/g,'');
+            // tipJS.innerHTML = `${e.target.value}`
+            // console.log(btn.innerHTML.replace(/%/g,""));
+            billTip = btn.innerHTML.replace(/%/g,"");
+            totalFunction(billTip);
+    })
+})
+};
+
+
+
+function peopleFunction(){
+
+    numbOfPeople.addEventListener("keyup",(e)=>{
+        console.log(e.target.value);
+    });
+};
+
+billFunction();
+tipFunction();
+peopleFunction();
+
+totalFunction();
+
+function totalFunction(bill,tip){
+    console.log(bill);
+    totalJS.innerHTML = `\$${bill}`
+
+}
+
+// totalMathFunction(billFunction,tipFunction);
+
+// function totalMathFunction(bill, tip){
+//  console.log(bill && tip)
+// }
