862293912513e552e6e7f972f94f11a86a2a07cd
RudyMathis <103608310+RudyMathis@users.noreply.github.com>
2022-06-15 15:29:39 -0400

    Added functioning custom button still need to adjust how it looks simple fix for reset button to work properly

--- main.js
+++ main.js
@@ -13,84 +13,89 @@ const tipJS = document.querySelector("#tip-js");
 const tipBtn = document.querySelectorAll(".tip-btn");
 const numbOfPeople = document.querySelector("#number-people");
 const resetBtn = document.querySelector(".reset");
+const custom = document.querySelector("#custom");
 
 
+
+// number of people
 function peopleFunction(){
-
     numbOfPeople.addEventListener("keyup",(e)=>{
     const numbPeopleAmount = `${Number(e.target.value)}`;
     numbPeopleAmount.innerHTML = `${numbOfPeople}`;
 
-    if(bill.value > 0){
-        const tipNumb = Number(billTip * .01 );
-
-        tipJS.innerHTML = `\$${parseFloat((billTotal * tipNumb) / Number(numbPeopleAmount)).toFixed(2)}`
-        totalJS.innerHTML =`\$${parseFloat((Number(billTotal) + (billTotal * tipNumb)) / Number(numbPeopleAmount)).toFixed(2)}`
-    }
-
+    if(bill.value >= 0){
+        outputFunction()
+        }
     });
 };
 
+
+// bill input
 bill.addEventListener("keyup",()=>{
     
     totalAmountFunction();
     peopleFunction();
-
-
 
     totalJS.innerHTML = `\$${billTotal}`
     if(totalJS.innerHTML === "$"){
         totalJS.innerHTML = "$0.00"
     } 
 
-    if(bill.value > 0){
+    if(bill.value >= 0){
         totalTipFunction();
     } else {
         totalJS.innerHTML = "$0.00"
     }
-
 })
 
-// peopleFunction();
-
+// totalperson
 function totalAmountFunction(){
+
     billTotal = bill.value;
     if(billTotal >= 0){
         
         resetFunction();
     }
+    
+
+}
+
+// totaltip/person
+function totalTipFunction(){
+    
+    tipBtn.forEach((btn) =>{
+        btn.addEventListener("click",()=>{
+            billTip = btn.innerHTML.replace(/%/g,"");
+            outputFunction()
+        })
+
+        custom.addEventListener("keyup",()=>{
+        customTip = custom.value;
+        outputFunction()
+        console.log(customTip)
+    })
+    })
+
 }
 
 
-function totalTipFunction(){
-
-    tipBtn.forEach((btn) =>{
-        btn.addEventListener("click",()=>{
-
-            billTip = btn.innerHTML.replace(/%/g,"");
-            const tipNumb = Number(billTip * .01 );
-            const numbPeopleAmount = Number(numbOfPeople.value);
-            tipJS.innerHTML = `\$${parseFloat((billTotal * tipNumb) / Number(numbPeopleAmount)).toFixed(2)}`
-            totalJS.innerHTML =`\$${parseFloat((Number(billTotal) + (billTotal * tipNumb)) / Number(numbPeopleAmount)).toFixed(2)}`
-
-        })
-    })
-};
-
-
+// reset
 function resetFunction(){
-
+    
     resetBtn.removeAttribute("disabled");
-
     resetBtn.addEventListener("click",()=>{
-        bill.value = 0;
-        bill.innerHTML = 0.00;
-        numbOfPeople.value = 1;
-        tipJS.innerHTML = 0.00;
-        totalJS.innerHTML = 00;
-        resetBtn.setAttribute("disabled", "")
-        console.log(totalJS)
-        })
+        window.location.reload()
+    })
+}
+
+
+function outputFunction(){
+    const tipNumb = Number(billTip * .01 );
+    const customTip = Number(custom.value * .01 );
+    const numbPeopleAmount = Number(numbOfPeople.value);
+
+    tipJS.innerHTML = `\$${parseFloat((billTotal * (tipNumb || customTip)) / Number(numbPeopleAmount)).toFixed(2)}`;
+    totalJS.innerHTML =`\$${parseFloat((Number(billTotal) + (billTotal * (tipNumb || customTip))) / Number(numbPeopleAmount)).toFixed(2)}`;
 }
 
 
